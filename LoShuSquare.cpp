205222e9685c7807b26837d08d6057da72dd1601
Michael Wallerius <mikesterciv1@gmail.com>
2020-03-13 21:36:48 -0700

    added comments, still need to work on EC

--- LoShuSquare.cpp
+++ LoShuSquare.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+//Sets all the values in the square to zero
 LoShuSquare::LoShuSquare()
 {
     for (int i=0; i < ROWS; i++)
@@ -10,6 +11,7 @@ LoShuSquare::LoShuSquare()
         squares[i][j] = 0;
 }
 
+//Prints the board in a 3x3 square. Does not return anything.
 void LoShuSquare::printBoard(){
     int cnt = 0;
     for (int i=0; i < ROWS; i++)
@@ -21,10 +23,12 @@ void LoShuSquare::printBoard(){
     }
 }
 
+//Fills each square sequentially (row1 then row2 then row3) with the user input. Does not return anything.
 void LoShuSquare::fill(int row, int col, int value){
     squares[row][col] = value;
 }
 
+//Private function that checks if the sum of all three rows adds to 45. Returns true or false.
 bool LoShuSquare::checkRow(){
     int sum = 0;
     for (int i=0; i < ROWS; i++)
@@ -35,6 +39,8 @@ bool LoShuSquare::checkRow(){
     else
         return false;
 }
+
+//Private function that checks if the sum of all three columns adds to 45. Returns true or false.
 bool LoShuSquare::checkCol(){
     int sum = 0;
     for (int i=0; i < ROWS; i++)
@@ -46,7 +52,7 @@ bool LoShuSquare::checkCol(){
         return false;
 }
 
-
+//Private function that checks if the diagonals are equal to each other. Returns true or false.
 bool LoShuSquare::checkDiag(){
     int leftDiag = squares[0][0] + squares[1][1] + squares[2][2];
     int rightDiag = squares[2][0] + squares[1][1] + squares[0][2];
@@ -56,7 +62,7 @@ bool LoShuSquare::checkDiag(){
         return false;
 }
 
-
+//Checks if all three private function checks returns true. Returns true or false.
 bool LoShuSquare::check(){
     if (checkRow() == true && checkCol() == true && checkDiag() == true)
         return true;
--- LoShuSquare.h
+++ LoShuSquare.h
@@ -1,3 +1,15 @@
+/*
+  CIS 22B
+
+    Lab6: Prompts user to fill out a 3x3 square with numbers 1-9
+    Determines if square is a LoShuMagic square
+    p.s. still working on the extra credit so test using Pacheco's main.cpp
+
+  Author: Michael Wallerius
+
+  Date: 3/13/2020
+*/
+
 #ifndef LOSHUSQUARE_H
 #define LOSHUSQUARE_H
 
